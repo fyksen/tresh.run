522d0a7beeb350b86e00b5f14f599e866550b7ae
fyksen <fredrik@fyksen.me>
2023-05-22 15:54:24 +0200

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -66,6 +66,6 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
-    'plausible': { domain: 'tresh.run', 'selfHostedUrl': 'plausible.tresh.run' },
+    'plausible' { domain: 'tresh.run', 'selfHostedUrl': 'plausible.tresh.run' },
   ]
 }
